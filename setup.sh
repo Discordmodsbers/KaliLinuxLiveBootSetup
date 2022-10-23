cd44bc70d6eb07b1927f95077c7dca61e3c4af2d
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-10-23 19:43:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,16 +5,20 @@ echo "HHHHHHH  aa aaa   zz   aa aaa rrr  r  dddddd  SSSSS  ee   e tttt  uu   uu 
 echo "HH   HH aa  aaa  zz   aa  aaa rr     dd   dd      SS eeeee  tt    uu   uu pppppp  oo  oo rr     "
 echo "HH   HH  aaa aa zzzzz  aaa aa rr      dddddd  SSSSS   eeeee  tttt  uuuu u pp       oooo  rr     "
 
-sudo apt update
-sudo apt install neofetch
-sudo apt install 
+sudo apt update -y
+sudo apt install neofetch -y 
+clear
 echo "-INSTALLING GAME-"
 pip3 install pygame
 git clone https://github.com/Discordmodsbers/Retr0Snake
+clear
 echo "-INSTALLING AUTODOXPASTER-"
 git clone https://github.com/Discordmodsbers/AutoDoxPaster
+clear
 echo "-INSTALLING SSHBRUTE-"
 pip3 install colorama 
 pip3 install paramiko
 git clone https://github.com/Discordmodsbers/sshbrute
+clear
 echo "-SETUP FINISHED ENJOY!-"
+ls
