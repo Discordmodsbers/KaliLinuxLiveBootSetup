2a147637459ea054722cc0ab6272ec715c6156e4
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-10-23 17:01:06 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+echo "-STARTING SETUP PLEASE WAIT-"
+echo HH   HH                                   dd  SSSSS         tt                                  
+echo HH   HH   aa aa zzzzz   aa aa rr rr       dd SS        eee  tt    uu   uu pp pp    oooo  rr rr  
+echo HHHHHHH  aa aaa   zz   aa aaa rrr  r  dddddd  SSSSS  ee   e tttt  uu   uu ppp  pp oo  oo rrr  r 
+echo HH   HH aa  aaa  zz   aa  aaa rr     dd   dd      SS eeeee  tt    uu   uu pppppp  oo  oo rr     
+echo HH   HH  aaa aa zzzzz  aaa aa rr      dddddd  SSSSS   eeeee  tttt  uuuu u pp       oooo  rr     
+                                                                          pp                    
+sudo apt update
+sudo apt install neofetch
+sudo apt install 
+echo "-INSTALLING GAME-"
+pip3 install pygame
+git clone https://github.com/Discordmodsbers/Retr0Snake
+echo "-INSTALLING AUTODOXPASTER-"
+git clone https://github.com/Discordmodsbers/AutoDoxPaster
+echo "-INSTALLING SSHBRUTE-"
+pip3 install colorama 
+pip3 install paramiko
+git clone https://github.com/Discordmodsbers/sshbrute
+echo "-SETUP FINISHED ENJOY!-"
